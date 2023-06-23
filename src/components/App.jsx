01c31f0934f28ba11b1cdc26f5cd00897aeef535
Taras Oliynik <112812514+Tarasevich25@users.noreply.github.com>
2023-06-23 22:27:03 +0300

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './Transaction/TransactionHistory';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+import Container from './Container/Container';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Container>
+        <Profile
+          name={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Container>
+      <Container>
+        <Statistics title="Upload stats" stats={data} />
+        {/* <Statistics stats={data} /> */}
+      </Container>
+      <Container>
+        <FriendList friends={friends} />
+      </Container>
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
+
+export default App;
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import FriendListItem from '../FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friend_list}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
+
+export default FriendList;
